7e7979423d7313580ce08ad8c4a6370a5d1a2731
igor5566 <59133419+igor5566@users.noreply.github.com>
2019-12-22 19:29:21 +0200

    Update BaseTest.java

--- src/test/java/mysite/BaseTest.java
+++ src/test/java/mysite/BaseTest.java
@@ -29,6 +29,6 @@ public class BaseTest {
     @AfterSuite
     public void closeBrowser() {
         driver.quit();
-        System.out.println("All test have been complied.");
+        System.out.println("Everything is okay.");
     }
 }
