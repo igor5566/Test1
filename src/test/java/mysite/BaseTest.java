cc78e1672b2bc8132ab7d8cfe24fe4531b549692
igor5566 <volkoff5566@ukr.net>
2019-12-22 18:52:08 +0200

    Add test with testNG

---
+++ src/test/java/mysite/BaseTest.java
@@ -0,0 +1,33 @@
+package mysite;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class BaseTest {
+    public static WebDriver driver;
+    public static ChromeOptions options;
+    public static WebDriverWait wait;
+
+    @BeforeSuite
+    public void setWebDriver() {
+        options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver,15);
+    }
+
+    @AfterSuite
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/mysite/GoogleTest.java
@@ -0,0 +1,34 @@
+package mysite;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
+import static org.testng.Assert.*;
+
+public class GoogleTest extends BaseTest {
+    @Test
+    public void testSearchForm() {
+        driver.get("http://google.com");
+        WebElement searchField = driver.findElement(By.cssSelector("[name='q']"));
+       // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        searchField.sendKeys("selenium", Keys.ENTER);
+        WebElement firstResult = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".srg .g:nth-child(1)")));
+        Assert.assertTrue(firstResult.getText().contains("Selenium"));
+
+        driver.findElement(By.name("q")).sendKeys(" chemical element", Keys.ENTER);
+        WebElement secondResult = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".srg .g:nth-child(1)")));
+        wait.until(textToBePresentInElementLocated(By.cssSelector(".srg"),"element"));
+
+     }
+
+}
